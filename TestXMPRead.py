23fd84afd36891d859775342f272eace8dc73db4
andrewcgaitskell <andrew@gaitskell.com>
2017-03-19 20:28:30 +0000

    Update TestXMPRead.py

--- TestXMPRead.py
+++ TestXMPRead.py
@@ -2,3 +2,6 @@ from libxmp import XMPFiles, consts
 xmpfile = XMPFiles( file_path="Pong.jpg" )
 xmp = xmpfile.get_xmp()
 print(xmp)
+xmpfile = XMPFiles( file_path="William.jpg" )
+xmp = xmpfile.get_xmp()
+print(xmp)
