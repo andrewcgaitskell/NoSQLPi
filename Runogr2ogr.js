4732cb34bbf5f238fc639c0e9c7696ffc75bbedb
andrewcgaitskell <andrew@gaitskell.com>
2017-03-25 16:32:33 +0000

    Update Runogr2ogr.js

--- Runogr2ogr.js
+++ Runogr2ogr.js
@@ -1,6 +1,6 @@
 var ogr2ogr = require('ogr2ogr')
 
-ogr2ogr('../test/samples/sample.shp.zip').exec(function(er, data) {
+ogr2ogr('HE_HighwayBoundary_v3_3_1.shp').exec(function(er, data) {
   if (er) console.error(er)
   console.log(data)
 })
