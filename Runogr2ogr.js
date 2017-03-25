eeaff7da8911d8d9452a0534ebf134a12433c449
andrewcgaitskell <andrew@gaitskell.com>
2017-03-25 16:45:10 +0000

    Update Runogr2ogr.js

--- Runogr2ogr.js
+++ Runogr2ogr.js
@@ -4,3 +4,5 @@ ogr2ogr('HE_HighwayBoundary_v3_3_1.shp').exec(function(er, data) {
   if (er) console.error(er)
   console.log(data)
 })
+
+ogr2ogr -f GeoJSON -t_srs crs:84 "HE_HighwayBoundary_v3_3_1.geojson" "HE_HighwayBoundary_v3_3_1.shp"
